6b6c6992adad509aa8ae86ef7fc242d1efe6cc0c
35596035 <william82733791@gmail.com>
2022-09-20 19:40:22 +0800

    test

--- 111-1PC4/Test2.aspx.cs
+++ 111-1PC4/Test2.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace _111_1PC4
 {
-    public partial class Test1 : System.Web.UI.Page
+    public partial class Test2 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
